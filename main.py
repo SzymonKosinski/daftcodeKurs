b44ed935635e4e2a1ca31c33adb30d917d5552fa
Szymon <kosinski.szymon@o2.pl>
2021-04-20 23:23:43 +0200

    zadanie 4

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException, Response
 import hashlib
+from datetime import date, timedelta
 from pydantic import BaseModel
+
+
+
 app = FastAPI()
 
 
@@ -34,4 +38,30 @@ def authorization(password ="", password_hash ="", response = Response):
     elif password=="":
         raise HTTPException(status_code=401, detail="unathorized password")
     return {"item": "lol"}
+
+class Register(BaseModel):
+    name: str
+    surname: str
+
+
+app.id_counter = 0
+app.registration = dict()
+@app.post("/register", status_code=201)
+def register(response: Response, register_person: Register):
+    register_date = date.today()
+    days_to_add = len(register_person.name) + len(register_person.surname)
+    vaccination_date = register_date + timedelta(days_to_add)
+
+    app.id_counter += 1
+    data_of_person_to_be_registered = dict()
+    data_of_person_to_be_registered["id"] = app.id_counter
+    data_of_person_to_be_registered["name"] = register_person.name
+    data_of_person_to_be_registered["surname"] = register_person.surname
+    data_of_person_to_be_registered["register_date"] = register_date
+    data_of_person_to_be_registered["vaccination_date"] = vaccination_date
+    app.registration[app.id_counter] = data_of_person_to_be_registered
+
+    return data_of_person_to_be_registered
+
+
 #uvicorn main:app
