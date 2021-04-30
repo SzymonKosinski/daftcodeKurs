b2823fa5ad6bb9f3c70d6a42c023ffd9f31538e0
Szymon <kosinski.szymon@o2.pl>
2021-04-30 13:44:50 +0200

    add base64 authentication on both endpoints and fixed

--- main.py
+++ main.py
@@ -111,33 +111,38 @@ def message():
 @app.post("/login_session", status_code=201)
 def logowanie(login: str=""):
     b=bytes(login, 'utf-8')
-    loginBase64 = str(base64.b64encode(b))
+    loginBase64 = base64.b64encode(b)
     klucz="4dm1n:NotSoSecurePa$$"
     b=bytes(klucz, 'utf-8')
-    kluczBase64=str(base64.b64encode(b))
-    if kluczBase64!=loginBase64:
-        raise HTTPException(status_code=401, detail="unathorized password")
-    else:
+    kluczBase64=base64.b64encode(b)
+    kluczBase64 = kluczBase64.decode('utf-8')
+    loginBase64 = loginBase64.decode('utf-8')
+    print(kluczBase64)
+    if kluczBase64==loginBase64 or login==kluczBase64:
         response = JSONResponse(content=loginBase64)
         response.set_cookie(key="session_token", value="starywiniary")
+    else:
+        raise HTTPException(status_code=401, detail="unathorized password")
 
 
 
 @app.post("/login_token", status_code=201 )
 def weryfikacja(login: str=""):
     b = bytes(login, 'utf-8')
-    loginBase64 = str(base64.b64encode(b))
+    loginBase64 = base64.b64encode(b)
     klucz = "4dm1n:NotSoSecurePa$$"
     b = bytes(klucz, 'utf-8')
-    kluczBase64 = str(base64.b64encode(b))
-    #print(kluczBase64)
-    if kluczBase64 != loginBase64 and login!=kluczBase64:
-        raise HTTPException(status_code=401, detail="unathorized password")
-    else:
+    kluczBase64 = base64.b64encode(b)
+    kluczBase64 = kluczBase64.decode('utf-8')
+    loginBase64 = loginBase64.decode('utf-8')
+    print(kluczBase64)
+    if kluczBase64 == loginBase64 or login==kluczBase64:
         token = klucz
         return "token:"f"{token}"
+    else:
+        raise HTTPException(status_code=401, detail="unathorized password")
 
 
-logowanie("4dm1n:NotSoSecurePa$$")
-weryfikacja("4dm1n:NotSoSecurePa$$")
+logowanie('4dm1n:NotSoSecurePa$$')
+weryfikacja("NGRtMW46Tm90U29TZWN1cmVQYSQk")
 # uvicorn main:app
