fc4a1f1120dcd4e6b1e4ded74cb261353ef84a9b
Szymon <kosinski.szymon@o2.pl>
2021-05-03 11:03:16 +0200

    próba znalezienia błędu

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+def number_of_letters(word):
+    result = 0
+    for character in word:
+        if character.isalpha():
+            result += 1
+    return result
+
+
+class Register(BaseModel):
+    name: str
+    surname: str
+
+
+app.id_counter = 0
+app.registration = dict()
 
 @app.get("/method")
 def method():
@@ -38,68 +53,12 @@ def root_post():
     return {"method": "POST"}
 
 
-@app.get("/auth", status_code=204)
-def authorization(password="", password_hash="", response=Response):
-    encrypted_password = hashlib.sha512(password.encode('utf-8')).hexdigest()
-    if encrypted_password != password_hash:
-        raise HTTPException(status_code=401, detail="unathorized password")
-    elif password == "":
-        raise HTTPException(status_code=401, detail="unathorized password")
-    return {"item": "lol"}
 
-
-def number_of_letters(word):
-    result = 0
-    for character in word:
-        if character.isalpha():
-            result += 1
-    return result
-
-
-class Register(BaseModel):
-    name: str
-    surname: str
-
-
-app.id_counter = 0
-app.registration = dict()
-
-
-@app.post("/register")
-def register(response: Response, register_person: Register):
-    register_date = date.today()
-    days_to_add = number_of_letters(register_person.name) + number_of_letters(register_person.surname)
-    vaccination_date = register_date + timedelta(days_to_add)
-
-    app.id_counter += 1
-
-    response.status_code = status.HTTP_201_CREATED
-    data_of_person_to_be_registered = dict()
-    data_of_person_to_be_registered["id"] = app.id_counter
-    data_of_person_to_be_registered["name"] = register_person.name
-    data_of_person_to_be_registered["surname"] = register_person.surname
-    data_of_person_to_be_registered["register_date"] = register_date
-    data_of_person_to_be_registered["vaccination_date"] = vaccination_date
-    app.registration[app.id_counter] = data_of_person_to_be_registered
-
-    return data_of_person_to_be_registered
-
-
-@app.get("/patient/{id}")
-def patient(response: Response, id: int):
-    if id > app.id_counter:  # nie ma takiego pacjenta
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return response.status_code
-    elif id < 1:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return response.status_code
-
-    return app.registration[id]
 
 
 @app.get("/hello", response_class=HTMLResponse)
 def message():
-    return '''
+    return ''' 
        <html>
            <head>
                <title>Some HTML in here</title>
@@ -107,15 +66,14 @@ def message():
            <body>
                <h1>Hello! Today date is {aktualnaData}</h1>
            </body>
-       </html>
-       '''.format(aktualnaData=date.today())
+       </html>'''.format(aktualnaData=date.today())
 
 
-@app.post("/login_session", status_code=201)
-def logowanie(login: str="", haslo: str=""):
+'''@app.post("/login_session", status_code=201)
+def logowanie(login: str="", haslo: str="",):
     response = Response()
     kluczLogin = "4dm1n"
-    kluczHaslo = 'NotSoSecurePa$$'
+    kluczHaslo = "NotSoSecurePa$$"
     print(login)
     print(haslo)
     if kluczLogin==login and kluczHaslo==haslo:
@@ -124,7 +82,7 @@ def logowanie(login: str="", haslo: str=""):
         token_login_session = "stary winiary"
         return response
     else:
-        raise HTTPException(status_code=401, detail="unathorized password")
+        raise HTTPException(status_code=401, detail="unathorized password")'''
 
 
 
@@ -146,6 +104,6 @@ def weryfikacja(login: str=""):
         raise HTTPException(status_code=401, detail="unathorized password")
 
 
-logowanie('4dm1n', 'NotSoSecurePa$$')
+logowanie("4dm1n", "NotSoSecurePa$$")
 weryfikacja("NGRtMW46Tm90U29TZWN1cmVQYSQk")
 # uvicorn main:app
