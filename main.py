ad964a9165444242de99009ad8689bffb5663706
Szymon <kosinski.szymon@o2.pl>
2021-05-11 00:40:05 +0200

    zadanie 4.1

--- main.py
+++ main.py
@@ -227,13 +227,19 @@ async def shutdown():
 async def categories(response: Response):
     cursor = await app.db_connection.execute("SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID")
     data = await cursor.fetchall()
+    print(data)
     return  {
         "categories": [
             {"id": int(x[0]), "name": f"{x[1]}"} for x in data
             ]
     }
 @app.get("/customers")
-def customers(response: Response):
-    pass
-
+async def customers(response: Response):
+    cursor = await app.db_connection.execute("SELECT CustomerID, CompanyName, Address || PostalCode || City || Country  FROM Customers ORDER BY CustomerID")
+    data = await cursor.fetchall()
+    return  {
+        "customers": [
+            {"id": f"{x[0]}", "name": f"{x[1]}", "full_adress": f"{x[2]}"} for x in data
+            ]
+    }
 # uvicorn main:app
