676250d5ad8304661272c599f0524eef5c2cb46b
Szymon <kosinski.szymon@o2.pl>
2021-05-11 01:05:17 +0200

    zadanie 4.1 test co jest nie tak

--- main.py
+++ main.py
@@ -227,7 +227,6 @@ async def shutdown():
 async def categories(response: Response):
     cursor = await app.db_connection.execute("SELECT CategoryID, CategoryName FROM Categories ORDER BY CategoryID")
     data = await cursor.fetchall()
-    print(data)
     return  {
         "categories": [
             {"id": int(x[0]), "name": f"{x[1]}"} for x in data
@@ -235,7 +234,7 @@ async def categories(response: Response):
     }
 @app.get("/customers")
 async def customers(response: Response):
-    cursor = await app.db_connection.execute("SELECT CustomerID, CompanyName, Address || ' ' || PostalCode || ' '  || City || ' ' || Country  FROM Customers ORDER BY CustomerID")
+    cursor = await app.db_connection.execute("SELECT CustomerID, CompanyName, (COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, '') || ' ' || COALESCE(Country, ''))  FROM Customers ORDER BY CustomerID")
     data = await cursor.fetchall()
     return  {
         "customers": [
