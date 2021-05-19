bbe46e56ce0de2247892425dead70aad73280256
Szymon <kosinski.szymon@o2.pl>
2021-05-19 02:00:25 +0200

    rebuild app

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 
